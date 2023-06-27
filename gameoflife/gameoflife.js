daa535665f357d3918108f345c6aac6e33fabd1f
Sam Colwill <57352809+SamColwill@users.noreply.github.com>
2023-06-27 15:28:25 +0100

    Finished game of life
    Added full life cycle (start, stop and reset), and the ability to add life forms to the canvas.

--- gameoflife/gameoflife.js
+++ gameoflife/gameoflife.js
@@ -1,5 +1,9 @@
 const worldwidth = 300;
 const worldheight = 150;
+var world;
+var nextworld;
+
+var running;
 
 //initialse both world and nextworld
 var world = new Array(worldwidth);
@@ -11,82 +15,24 @@ for (var i = 0; i < world.length; i++) {
     nextworld[i] = new Array(worldheight);
 }
 
-//set every cell to false in both world and nextworld
-for (var i = 0; i < worldwidth; i++) {
-    for (var j = 0; j < worldheight; j++) {
-        world[i][j] = false;
-    }
-}
-for (var i = 0; i < worldwidth; i++) {
-    for (var j = 0; j < worldheight; j++) {
-        nextworld[i][j] = false;
-    }
-}
+emptyworld();
 
-//Testing Shapes
-
-//still lifes - block
-///*
-world[100][75] = true;
-world[101][75] = true;
-world[100][76] = true;
-world[101][76] = true;
-//*/
-
-//still lifes - bee-hive
-///*
-world[53][50] = true;
-world[54][50] = true;
-world[52][51] = true;
-world[55][51] = true;
-world[53][52] = true;
-world[54][52] = true;
-//*/
-
-//oscillators - blinker
-///*
-world[149][155] = true;
-world[149][156] = true;
-world[149][157] = true;
-//*/
-
-//oscillators - toad
-///*
-world[53][50] = true;
-world[54][50] = true;
-world[52][51] = true;
-world[55][51] = true;
-world[53][52] = true;
-world[54][52] = true;
-//*/
-
-//spaceships - glider
-///*
-world[249][155] = true;
-world[249][156] = true;
-world[249][157] = true;
-world[248][157] = true;
-world[247][156] = true;
-//*/
-
-function gameoflifetick() {
-    console.log("run gameoflife");
+function emptyworld() {
+//sets every cell to false in both world and nextworld
     for (var i = 0; i < worldwidth; i++) {
         for (var j = 0; j < worldheight; j++) {
-            aliveordead(i, j);
+            world[i][j] = false;
         }
     }
     for (var i = 0; i < worldwidth; i++) {
         for (var j = 0; j < worldheight; j++) {
-            world[i][j] = nextworld[i][j];
+            nextworld[i][j] = false;
         }
     }
-    gameoflifedisplay();
-    console.log(world);
-    console.log(nextworld);
 }
 
 function aliveordead(x, y) {
+//function to determine whether each cell is dead or alive
     var aliveneighbours = 0
     for (var i = -1; i <= 1; i++) {
         for (var j = -1; j <= 1; j++) {
@@ -108,32 +54,146 @@ function aliveordead(x, y) {
 
     if (world[x][y] == true && aliveneighbours < 2) {
     //if alive cell has less than two alive neighbours then it dies.
-        console.log("(" + x + "," + y + ") died undercrowding. aliveneighbours:" + aliveneighbours);
+        //console.log("(" + x + "," + y + ") died undercrowding. aliveneighbours:" + aliveneighbours);
         nextworld[x][y] = false;
     }
     
     if (world[x][y] == true && (aliveneighbours == 2 || aliveneighbours == 3)) {
     //if alive cell has two or three alive neightbours then it stays alive.
-        console.log("(" + x + "," + y + ") stayed alive. aliveneighbours:" + aliveneighbours);
+        //console.log("(" + x + "," + y + ") stayed alive. aliveneighbours:" + aliveneighbours);
         nextworld[x][y] = true;
     }
 
     if (world[x][y] == true && aliveneighbours > 3) {
     //if alive cell has more than three alive neightbours then it dies.
-        console.log("(" + x + "," + y + ") died overcrowding. aliveneighbours:" + aliveneighbours);
+        //console.log("(" + x + "," + y + ") died overcrowding. aliveneighbours:" + aliveneighbours);
         nextworld[x][y] = false;
     }
 
     if (world[x][y] == false && aliveneighbours == 3) {
     //if dead cell has exactly three alive neightbours then it becomes alive.
-        console.log("(" + x + "," + y + ") alived. aliveneighbours:" + aliveneighbours);
+        //console.log("(" + x + "," + y + ") alived. aliveneighbours:" + aliveneighbours);
         nextworld[x][y] = true;
     }
 }
 
-//all following fucntions handle dispalying gameoflife
+function gameoflifetick() {
+//one tick/generation of the game of life
+    console.log("gameoflife tick");
+    for (var i = 0; i < worldwidth; i++) {
+        for (var j = 0; j < worldheight; j++) {
+            aliveordead(i, j);
+        }
+    }
+    for (var i = 0; i < worldwidth; i++) {
+        for (var j = 0; j < worldheight; j++) {
+            world[i][j] = nextworld[i][j];
+        }
+    }
+    gameoflifedisplay();
+}
 
+function startgameoflife() {
+    running = setInterval(gameoflifetick, 200);
+}
+
+function stopgameoflife() {
+    clearInterval(running);
+}
+
+function resetgameoflife() {
+    stopgameoflife();
+    emptyworld();
+    gameoflifedisplay();
+}
+
+function addlife(lifeform) {
+    stopgameoflife();
+    var x = Math.floor(Math.random() * (worldwidth - 20)) + 10;
+    var y = Math.floor(Math.random() * (worldheight - 20)) + 10;
+
+    switch(lifeform) {
+        case 'block':
+            world[x][y] = true;
+            world[x + 1][y] = true;
+            world[x][y + 1] = true;
+            world[x + 1][y + 1] = true;
+            console.log("added block at (" + x + "," + y + ")");
+            break;
+        case 'toad':
+            world[x + 2][y] = true;
+            world[x][y + 1] = true;
+            world[x + 3][y + 1] = true;
+            world[x][y + 2] = true;
+            world[x + 3][y + 2] = true;
+            world[x + 1][y + 3] = true;
+            console.log("added toad at (" + x + "," + y + ")");
+            break;
+        case 'light-weight spaceship':
+            world[x + 2][y] = true;
+            world[x + 3][y] = true;
+            world[x][y + 1] = true;
+            world[x + 1][y + 1] = true;
+            world[x + 3][y + 1] = true;
+            world[x + 4][y + 1] = true;
+            world[x][y + 2] = true;
+            world[x + 1][y + 2] = true;
+            world[x + 2][y + 2] = true;
+            world[x + 3][y + 2] = true;
+            world[x + 1][y + 3] = true;
+            world[x + 2][y + 3] = true;
+            console.log("added light-weight spaceship at (" + x + "," + y + ")");
+            break;
+        case 'glider gun':
+            if (x > worldwidth - 30) {
+                x -= 20;
+            }
+            world[x + 24][y] = true;
+            world[x + 22][y + 1] = true;
+            world[x + 24][y + 1] = true;
+            world[x + 12][y + 2] = true;
+            world[x + 13][y + 2] = true;
+            world[x + 20][y + 2] = true;
+            world[x + 21][y + 2] = true;
+            world[x + 34][y + 2] = true;
+            world[x + 35][y + 2] = true;
+            world[x + 11][y + 3] = true;
+            world[x + 15][y + 3] = true;
+            world[x + 20][y + 3] = true;
+            world[x + 21][y + 3] = true;
+            world[x + 34][y + 3] = true;
+            world[x + 35][y + 3] = true;
+            world[x][y + 4] = true;
+            world[x + 1][y + 4] = true;
+            world[x + 10][y + 4] = true;
+            world[x + 16][y + 4] = true;
+            world[x + 20][y + 4] = true;
+            world[x + 21][y + 4] = true;
+            world[x][y + 5] = true;
+            world[x + 1][y + 5] = true;
+            world[x + 10][y + 5] = true;
+            world[x + 14][y + 5] = true;
+            world[x + 16][y + 5] = true;
+            world[x + 17][y + 5] = true;
+            world[x + 22][y + 5] = true;
+            world[x + 24][y + 5] = true;
+            world[x + 10][y + 6] = true;
+            world[x + 16][y + 6] = true;
+            world[x + 24][y + 6] = true;
+            world[x + 11][y + 7] = true;
+            world[x + 15][y + 7] = true;
+            world[x + 12][y + 8] = true;
+            world[x + 13][y + 8] = true;
+            console.log("added glider gun at (" + x + "," + y + ")");
+            break;
+    }
+
+    gameoflifedisplay();
+}
+
+document.addEventListener("DOMContentLoaded", gameoflifedisplay);
 function gameoflifedisplay() {
+//displays the game of life on a canvas on the webpage
     var gameoflifecanvas = document.getElementById("gameoflifecanvas");
     var ctx = gameoflifecanvas.getContext("2d");
 
@@ -150,6 +210,3 @@ function gameoflifedisplay() {
         }
     }
 }
-
-document.addEventListener("DOMContentLoaded", gameoflifedisplay);
-//Run above fucntion once the page has loaded.
